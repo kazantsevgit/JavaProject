92cc6967d21ef163e7820ae855076bd78f151897
Kristal-LID <savelii_iakovlev@icloud.com>
2025-05-07 20:52:23 +0500

    dto1

---
+++ CryptoShop/src/main/java/com/example/cryptoshop/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package com.example.cryptoshop.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class UserDTO {
+    public long id;
+    public String name;
+}
