fc1498f191c166efafe82a3d1ea79c9e447eb483
Никита <160343939+kazantsevgit@users.noreply.github.com>
2025-05-07 20:36:38 +0500

    Spring Boot

---
+++ CryptoShop/src/main/java/com/example/cryptoshop/CryptoShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoShopApplication.class, args);
+	}
+
+}
---
+++ CryptoShop/src/test/java/com/example/cryptoshop/CryptoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
